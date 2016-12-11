43b0e2a0b0856b850c29ad90087cd3a804c3dcda
Allan-Contaret <allancontaret@MacBook-Pro-de-Allan.local>
2016-12-11 20:57:29 +0100

    Initial Commit

---
+++ O'Resto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  O'Resto
+//
+//  Created by Allan Contaret on 11/12/2016.
+//  Copyright © 2016 Allan Contaret. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
